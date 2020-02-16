e0afb732fdcea43da2092074306a943a1394555d
Adam ASU <56737464+akclifto@users.noreply.github.com>
2020-02-16 01:18:33 -0700

    Update twosum_brute.c

--- twosum_brute.c
+++ twosum_brute.c
@@ -3,6 +3,8 @@
  * Note: The returned array must be malloced, assume caller calls free().
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ *RUNTIME: 59ms
  */
 
 #include<stdio.h>
