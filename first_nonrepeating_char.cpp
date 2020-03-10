c2cf5bcac086458ed719ffa722d8d6aca6d5d7db
Adam ASU <56737464+akclifto@users.noreply.github.com>
2020-03-09 20:28:17 -0700

    adding egg drop use to main

--- first_nonrepeating_char.cpp
+++ first_nonrepeating_char.cpp
@@ -52,19 +52,58 @@ void check_string(char *str, int index)
 		cout << "First non-repeating character in the given string is " << str[index] << ".\n";
 }
 
+void display_egg_puzzle(eggDrop_puzzle egg, int eggs, int floors)
+{
+	cout << "The minimum number of trials in worst case: \n";
+	cout << eggs << " eggs\n";
+	cout << floors << " floors\n";
+	cout << "Result:  " << egg.egg_drop(eggs, floors) << ".\n";
+	return;
+}
+
+void display_egg_puzzle2D(eggDrop_puzzle egg, int eggs, int floors)
+{
+	cout << "\nThe minimum number of trials in worst case: \n";
+	cout << eggs << " eggs\n";
+	cout << floors << " floors\n";
+	cout << "Result:  " << egg.egg_drop_2D(eggs, floors) << ".\n";
+	return;
+}
+
 
 int main()
 {
+
+// first non-repeating char exercise--------------------------------------------------------------
+
+	cout << "FIRST NON-REPEATING CHARACTER EXERCISE\n\n";
 	//check string, should return n
 	char str[] = "this is the end?";
 	int index = first_nonRepeater(str);
 	check_string(str, index);
 
-	//check string, no non-repeating strings
+	//check string, no repeating strings
 	char str2[] = "aaaaaa";
 	index = first_nonRepeater(str2);
 	check_string(str2, index);
 
+// EGG DROPPING PUZZLE------------------------------------------------------------------------------
+
+	cout << "\n\n\nEGG DROPPING PUZZLE \n";
+
+	int eggs = 3;		 // number of eggs
+	int floors = 19;	 // number of floors
+	eggDrop_puzzle egg, egg_2D;
+	display_egg_puzzle(egg, eggs, floors);
+
+	//alternative way with 2D array sample - BROKEN
+	// eggs = 2;
+	// floors = 36;
+	// eggDrop_puzzle egg2;
+	// display_egg_puzzle(egg2, eggs, floors);
+	// display_egg_puzzle2D(egg_2D, eggs, floors);
+	
+	// egg_2D.destroy_egg_puzzle(egg_2D.egg_floor);
 	//getchar();
 	return 0;
 
