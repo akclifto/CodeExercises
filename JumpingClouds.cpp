6d3594f3879b9286878bcf11e1a211d951724a95
Adam ASU <56737464+akclifto@users.noreply.github.com>
2020-03-17 22:09:29 -0700

    formatting

--- JumpingClouds.cpp
+++ JumpingClouds.cpp
@@ -15,7 +15,7 @@
  * 		Sample Output:
  *			4
  *
- *		SOLUTION: traverse vector, if c = 0, walk by incrementing i in if-statement, else jump. 
+ *	SOLUTION: traverse vector, if c = 0, walk by incrementing i in if-statement, else jump. 
  */
 
 #include <iostream>
@@ -26,32 +26,30 @@ using namespace std;
 int jumpingOnClouds(vector<int> c);
 
 int main()
-{
-    
+{   
 	vector<int> c = { 0,0,1,0,0,1,0 };
 	int n = 7;
-    int result = jumpingOnClouds(c);
-    cout << result << endl; // should output 4
+	int result = jumpingOnClouds(c);
+    	cout << result << endl; // should output 4
 
-    vector<int> d = { 0,1,0,1,1,0,0,1,0,1,1,0,0 };
-    n = d.size();
-    result = jumpingOnClouds(d);
-    cout << result << endl; // should output 8
+    	vector<int> d = { 0,1,0,1,1,0,0,1,0,1,1,0,0 };
+    	n = d.size();
+    	result = jumpingOnClouds(d);
+    	cout << result << endl; // should output 8
 }
 
 
-int jumpingOnClouds(vector<int> c) {
-
-    int jumps = 0;
-
-    for (int i = 0; i < c.size() - 1; i++)
-    {
+int jumpingOnClouds(vector<int> c) 
+{
+    	int jumps = 0;
+	
+    	for (int i = 0; i < c.size() - 1; i++)
+    	{
 		if(c[i] == 0)
 		{
 			i++;
 		}
-        jumps++;
-    }
-    return jumps;
-
+        	jumps++;
+    	}
+    	return jumps;
 }
