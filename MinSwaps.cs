c6dafc2b3f63892e157cf3249518b08fce642b0b
Adam ASU <56737464+akclifto@users.noreply.github.com>
2020-09-01 12:05:05 -0700

    Minimum Swaps to Sort Array Problem
    Hackerrank

---
+++ MinSwaps.cs
@@ -0,0 +1,77 @@
+/**
+You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+
+For example, given the array  we perform the following steps:
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+It took 5 swaps to sort the array.
+
+
+*/
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+
+        int count = 0;
+
+        for(int i = 0; i < arr.Length - 1; i++) {
+
+            int current = arr[i];
+
+            while(current != i + 1){
+
+                current = swap(arr, current - 1, current);
+                count++;
+            }
+        }
+        return count;   
+    }
+
+    static int swap(int[] arr, int pos1, int pos2){
+
+        int temp = arr[pos1];
+        arr[pos1] = pos2; 
+        return temp;
+
+        // int temp = arr[current - 1];
+        // arr[current - 1] = current;
+        // current = temp;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
